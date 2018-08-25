d117e738a209f38814170a6db9ef56f0a836ec4e
Gilberto Hernandez <ghdz89@gmail.com>
2018-08-25 17:30:10 -0400

    experiment with onClick animation (pulse)

--- src/Square.js
+++ src/Square.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import './Square.css';
 
 class Square extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      active: false
+    };
+  }
+
+  handleClick(){
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+
+
   render() {
     return (
-      <div className="Square" style={{background: this.props.color}}>
+      <div className={this.state.active ? 'Squarepulse': 'Square'} style={{background: this.props.color}} onClick={this.handleClick}>
       </div>
     );
   }
